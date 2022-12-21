4bbc7e347734d80638562d3e48c1a2ea6fed9935
pronit29 <pronit29@gmail.com>
2022-12-22 00:05:16 +0530

    Adding the AssertionBasics file

--- src/main/java/AssertionBasics.java
+++ src/main/java/AssertionBasics.java
@@ -1,8 +1,9 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 //Validate if Add Place API is working fine as expected
-public class AddPlace {
+public class AssertionBasics {
     public static void main(String args[]) {
         final String jsonBody = "{\r\n"
                 + "  \"location\": {\r\n"
@@ -33,7 +34,9 @@ public class AddPlace {
                 .when().post(resourceURI)
 
                 //Then
-                .then().log().all().assertThat().statusCode(200);
+                .then().log().all().assertThat().statusCode(200)
+                .body("scope", equalTo("APP")) // This line is validating an element from the response body
+                .header("Server", equalTo("Apache/2.4.41 (Ubuntu)")); // This line is validating an element from the header
 
     }
 }
